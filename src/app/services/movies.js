8db01ffbf65a1827f172cc33863fe34c178f9b19
Simone Potenza <simone.potenza@gmail.com>
2017-08-13 23:04:30 +0200

    Connected dapp to contract

--- src/app/services/movies.js
+++ src/app/services/movies.js
@@ -1,35 +1,69 @@
 /* @flow */
 import Web3 from 'web3';
+import contract from 'truffle-contract';
+import abiSciFi from '../../../build/contracts/Scifi.json';
 import type {
-  ServicesConfig,
   MoviesService,
   MoviesServiceGetMoviesResponse,
   Movie,
   MoviesServiceVoteMovieResponse,
 } from '../flowtypes/services';
 
-export default function create({ web3Host }: ServicesConfig): MoviesService {
-  const web3 = new Web3.providers.HttpProvider(web3Host);
-  let movies: Array<Movie> = [{ title: 'movie 1' }, { title: 'movie 2' }];
+export default function create(): MoviesService {
+  let web3Provider;
+  let myWeb3;
+  let SciFiContract;
 
+  if (typeof web3 !== 'undefined') {
+    web3Provider = web3.currentProvider;
+    myWeb3 = new Web3(web3Provider);
+  } else {
+    // we try to use localhost
+    web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+    myWeb3 = new Web3(web3Provider);
+  }
+
+  SciFiContract = contract(abiSciFi);
+  SciFiContract.setProvider(web3Provider);
+
+  console.log('abiSciFi', abiSciFi);
+  console.log('contract', SciFiContract);
+  console.log(myWeb3);
+  console.log(Web3);
+  console.log(web3);
   return {
-    getMovies: (): Promise<MoviesServiceGetMoviesResponse> =>
-      new Promise((resolve) => {
-        setTimeout(() => {
-          resolve(movies);
-        }, 2000);
-      }),
-    voteMovie: ({ title }: Movie): Promise<MoviesServiceVoteMovieResponse> =>
-      new Promise((resolve) => {
-        setTimeout(() => {
-          const movie = movies.find(m => m.title === title);
-          if (movie) {
-            return resolve(movie);
-          }
-          const newMovie: Movie = { title };
-          movies = [...movies, newMovie];
-          return resolve(newMovie);
-        }, 2000);
-      }),
+    getMovies: async (): Promise<MoviesServiceGetMoviesResponse> => {
+      try {
+        const instance = await SciFiContract.deployed();
+        const movies = await instance.getMovies.call();
+        console.log('movies', movies);
+        const parsedMovies = movies.map(movie => ({
+          title: myWeb3.toAscii(movie).replace(/\0/g, ''),
+        }));
+
+        console.log('parsedMovies', parsedMovies);
+
+        return parsedMovies;
+      } catch (err) {
+        console.log('Error getting movies', err);
+      }
+    },
+    voteMovie: async ({ title }: Movie): Promise<MoviesServiceVoteMovieResponse> => {
+      try {
+        const instance = await SciFiContract.deployed();
+        debugger;
+        const transaction = await instance.vote(myWeb3.toHex(title), {
+          from: myWeb3.eth.accounts[0],
+          value: myWeb3.toWei(0.00002, 'ether'),
+        });
+        console.log('transaction', transaction);
+
+        return {
+          title,
+        };
+      } catch (err) {
+        console.log('Error voring movie', err);
+      }
+    },
   };
 }
